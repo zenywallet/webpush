2c9144023b942ec4b2a4de029c5abca0ec67e541
zenywallet <zenywallet@bitzeny.jp>
2024-02-28 00:52:47 +0900

    change vapid jws sign hash

--- src/webpush/vapid.nim
+++ src/webpush/vapid.nim
@@ -3,6 +3,8 @@
 import std/base64
 import std/json
 import std/strutils
+import nimcrypto
+import bytes
 
 var jwsHeaderJson = %*{"typ":"JWT","alg":"ES256"}
 var jwsPayloadJson = %*{"aud":"https://push.services.mozilla.com","sub":"mailto:admin@example.com","exp":"1463001340"}
@@ -13,4 +15,10 @@ var jwsHeader = base64.encode($jwsHeaderJson, true)
 jwsHeader.removeSuffix('=')
 var jwsPayload = base64.encode($jwsPayloadJson, true)
 jwsPayload.removeSuffix('=')
-echo jwsHeader & "." & jwsPayload
+
+var jwsSigningInput = jwsHeader & "." & jwsPayload
+
+proc sha256s*(data: openarray[byte]): array[32, byte] {.inline.} =
+  sha256.digest(data).data
+
+echo sha256s(cast[seq[byte]](jwsSigningInput))
